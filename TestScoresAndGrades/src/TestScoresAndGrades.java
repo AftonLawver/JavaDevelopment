943a4256bc9cd05a8bfa87ce87a12996e0b86c2e
afton21 <lawverap25@gmail.com>
2021-02-14 01:12:33 -0600

    Added do-while loop to homework #2 so the program automatically restarts if input is invalid.

--- TestScoresAndGrades/src/TestScoresAndGrades.java
+++ TestScoresAndGrades/src/TestScoresAndGrades.java
@@ -12,55 +12,61 @@ public class TestScoresAndGrades {
 		double score3 = -1;
 		boolean sentinel = false;
 		
-		myScanner = new Scanner(System.in);
-		try {
-			System.out.println("Enter the value for score 1: ");
-			score1 = myScanner.nextDouble(); 
-					
-			System.out.println("Enter the value for score 2: ");
-			score2 = myScanner.nextDouble(); 
+		do 
+		{
 			
-			System.out.println("Enter the value for score 3: ");
-			score3 = myScanner.nextDouble();
-			sentinel = true;
-		}
-		catch (InputMismatchException e) {
-			System.out.println("Please provide a valid number.");
-		}
-		 
-		if (sentinel == true) {
-			average = (score1 + score2 + score3)/3;
-			System.out.println("The average percentage for the three tests is " + average);
-			
-			if (average >= 90) {
-				grade = 'A';
+			myScanner = new Scanner(System.in);
+			try {
+				System.out.println("Enter the value for score 1: ");
+				score1 = myScanner.nextDouble(); 
+						
+				System.out.println("Enter the value for score 2: ");
+				score2 = myScanner.nextDouble(); 
+				
+				System.out.println("Enter the value for score 3: ");
+				score3 = myScanner.nextDouble();
+				sentinel = true;
 			}
-			
-			else if (average >= 80 && average < 90) {
-				grade = 'B';
+			catch (InputMismatchException e) {
+				System.out.println("Please provide a valid number.");
 			}
-			
-			else if (average >= 70 && average < 80) {
-				grade = 'C';
-			}
-			
-			else if (average >= 60 && average < 70) {
-				grade = 'D';
-			}
-			
+			 
+			if (sentinel == true) {
+				average = (score1 + score2 + score3)/3;
+				System.out.println("The average percentage for the three tests is " + average);
+				
+				if (average >= 90) {
+					grade = 'A';
+				}
+				
+				else if (average >= 80 && average < 90) {
+					grade = 'B';
+				}
+				
+				else if (average >= 70 && average < 80) {
+					grade = 'C';
+				}
+				
+				else if (average >= 60 && average < 70) {
+					grade = 'D';
+				}
+				
+				else {
+					grade = 'F';
+				}
+				
+				System.out.println("Your average grade for the three exams is " + grade);
+				myScanner.close();
+			}	
 			else {
-				grade = 'F';
+				System.out.println("Try again.");
 			}
-			
-			System.out.println("Your average grade for the three exams is " + grade);
-			
+
 		}
-		else {
-			System.out.println("Try again.\nProgram Exiting...");
-		}
-		
-		myScanner.close();
+		while (sentinel == false);
 		
 	}
-
+	
 }
+
+
