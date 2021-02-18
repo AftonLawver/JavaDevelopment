6e5fc6d1d212d6fa69b7d5ed5a12ce5f0825eba4
afton21 <lawverap25@gmail.com>
2021-02-17 22:19:58 -0600

    Created a new personal project. Program deals with reading and writing files.

--- ReadingAndWritingFiles/src/tester/ReadingAndWritingFiles.java
+++ ReadingAndWritingFiles/src/tester/ReadingAndWritingFiles.java
@@ -1,5 +1,7 @@
 package tester;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.io.File;
@@ -8,53 +10,54 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.swing.JOptionPane;
+import java.io.IOException;
 
 public class ReadingAndWritingFiles {
 
 	public static void main(String[] args) {
-		 
-		// export that file (basically create a new file from an old file)
+		// Create a blank txt file named names.txt
+		// ask the user for 5 names to add to the file
+		// once the user inputs 5 names, add these names to the file
+		// after adding them, ask the user to enter his/her name
+		// change the 3rd name to his/her name
+		// save the file.
 		
+		// Create the text file if it does not already exist
 		try {
-
-			File file = new File("names.txt");
+			File fileName = new File("names.txt");
 		
-			if (!file.exists()) {
-				file.createNewFile();
-			}
+		if (fileName.createNewFile()) {
+			System.out.println("File Created: " + fileName.getName());
 			
-			else {
-				Scanner myScanner = new Scanner(file);
+		}
+		else {
+			System.out.println("File already exists.");
+		}
 		
-				String fileContents = "This is a new file made by java" + "\n";
-		
-				while (myScanner.hasNextLine()) {
-			
-					fileContents = fileContents.concat(myScanner.nextLine() + "\n");
-				}
-		
-				PrintWriter writer = new PrintWriter("newnames.txt");
-				writer.println(fileContents);
-				writer.println("Alliyah");
-				writer.close();
-				myScanner.close();
-				System.out.println("Done");
-				
-			}
-			 
-		
-		
-				
-		
-			}
-		catch (IOException e) {
-			
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
 		
-	}
-	
+		// Ask the user for input of five names
+		// Another idea for this part is to create a method
+		// that loops through 5 times asking for a name. (for loop).
+		Scanner myScanner = new Scanner(System.in);
+		List names = new ArrayList();
+		do {
+			System.out.println("Enter a name: ");
+			String name = myScanner.nextLine();
+			names.add(name);
+		}while (names.size() < 5);
+		System.out.println(names);
+		
+		// take the list of names and write them to the newly created file
+		
+		
+		
 }
+}
+
 		
 		
 		
