79fefa3cc2309068744305cade15b7a4627db323
Afton Lawver <afton.terminalai@gmail.com>
2021-03-02 15:38:40 -0600

    Finished Exam 1. (Program that takes a user input of a year and determines whether or not that year is a leap year).

---
+++ Exam1/src/Exam1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		int year;
+		// Create a new scanner object called myScanner 
+		// to take user input for the year
+		Scanner myScanner = new Scanner(System.in);
+		System.out.print("Enter a year: ");
+		// Try block for exception handling in case user does not
+		// give a valid integer as input
+		try {
+			year = myScanner.nextInt();
+		// If method isLeapYear returns true, then print out 
+		// the year is a leap year.
+		if (isLeapYear(year)) {
+			System.out.println(year + " is a leap year.");
+			}
+		else {
+			System.out.println(year + " is not a leap year.");
+			}
+		}
+		// Catch the exception and prompt the user to provide valid integer
+		catch (Exception e) {
+			System.out.println("Please provide valid integer.");
+		}
+	}
+	
+	// Method that check if a year is a leap year or not
+	// Made method private because it only needs to be accessible by 
+	// this class. 
+	private static boolean isLeapYear(int year) {
+		// Initialize variables needed
+		if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+			return true;
+		}
+		else {
+			return false;
+		}		
+	}
+}
