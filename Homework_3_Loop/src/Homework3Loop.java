f1821e4da603a0ba02248aded59f12e4e51ba7a1
Afton Lawver <afton.terminalai@gmail.com>
2021-03-02 11:58:59 -0600

    Revert "Added comments to homework #3 (using ArrayList and Collections class)."
    This reverts commit bf7dabe5363bc2d6a8852b9e7ecdec352f69fe37.

--- Homework_3_Loop/src/Homework3Loop.java
+++ Homework_3_Loop/src/Homework3Loop.java
@@ -8,65 +8,55 @@ import java.util.Scanner;
 public class Homework3Loop {
 
 	public static void main(String[] args) {
-		// Create an ArrayList that will hold the values the user inputs
-		// Using an ArrayList because it contains methods like add(), min(), and max()
-		// which will be used later in the program.
-		// Create an ArrayList object that will be composed of integer objects called numbers.
-		// The ArrayList class does not take values like int.
-		List<Integer> numbers = new ArrayList();
+		List numbers = new ArrayList();
 		int largest;
 		int number;
 		int smallest;
 		int counter = 0;
 		
 		do {
-			// if the counter is greater or equal to 1, then ask prompt the user to enter another number.
 			if (counter >= 1) {
 				Scanner myScanner = new Scanner(System.in);
 				System.out.println("Enter another number or -1 to exit: ");
 				number = myScanner.nextInt();
 				if (number == -1) {
 					break;
-					}
-				else {
-					// Create an Integer object from the int user input by using the integer class
-					Integer x = Integer.valueOf(number);
-					// Use the method add() from the ArrayList class to add the Integer object to the ArrayList
-					numbers.add(x);
-					counter ++;
 				}
+				numbers.add(number);
+				counter ++;
+				myScanner.close();
 			}
+		
 			else {
+			
+					
 				Scanner myScanner = new Scanner(System.in);
 				System.out.println("Enter a number or -1 to exit: ");
 				number = myScanner.nextInt();
+				
 					if (number == -1) {
-						break;
-						}
-					else {
-						// Create an Integer object from the int user input by using the integer class
-						Integer x = Integer.valueOf(number);
-						// Use the method add() from the ArrayList class to add the Integer object to the ArrayList
-						numbers.add(number);
-						counter ++;
+					break;
 					}
+
 				}
+				numbers.add(number);
+				counter ++;
+
 		}		
 		while (number != -1);
-		// use the size method from the ArrayList class to get the size
-		// if there are no values in the ArrayList, then print no numbers provided.
-		// This will avoid getting a NullPointerException by not allowing a null ArrayList to be passed to the min and max methods.
+		
+		//Sort the list
+		Collections.sort(numbers); 
 		if (numbers.size() == 0) {
 			System.out.println("No numbers provided. Please try again.");
 		}
 		else {
-			// Use the Collections class, which comes from the java.util package.
-			// This class consists of static methods that operate on or return collections.
-			// https://docs.oracle.com/javase/8/docs/technotes/guides/collections/overview.html
-			// Find the largest value in the list
-			largest = Collections.max(numbers); 
-			//Find the smallest value in the list
-			smallest = Collections.min(numbers);
+			// Find the largest of the list
+			largest = (int) numbers.get(numbers.size() - 1); 
+			
+			//Find the smallest of the list
+			smallest = (int) numbers.get(0);
+			
 			// Display the largest and smallest to the user
 			System.out.println("The largest number you entered is: " + largest);
 			System.out.println("The smallest number you entered is: " + smallest);
