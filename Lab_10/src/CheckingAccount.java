000f7e02ef6b36b802c21c6fc65396708aa95552
Afton Lawver <afton.terminalai@gmail.com>
2021-04-16 16:54:59 -0500

    Finished calling the parent constructor.

--- Lab_10/src/CheckingAccount.java
+++ Lab_10/src/CheckingAccount.java
@@ -2,4 +2,13 @@
 public class CheckingAccount extends BankAccount{
 	static double FEE = 0.15;
 	
+	// Constructor
+	public CheckingAccount(String name, double initialAmount) {
+		super(); // invokes the parent class constructor 
+		String actnum = this.getAccountNumber();
+		String newAccountNumber = actnum + "-10";
+		this.setAccountNumber(newAccountNumber);
+		
+	}
+	
 }
