e22701eb907dff6a5f9c1b93f42d44b18b0c9239
afton21 <lawverap25@gmail.com>
2021-05-02 22:56:51 -0500

    Added some code to the Array (min, max, average of an array).

--- UserCreatedArray/src/Main.java
+++ UserCreatedArray/src/Main.java
@@ -22,9 +22,31 @@ public class Main {
 		for(int i = 0; i < tests.length; i++) {
 			System.out.println("Test " + (i+1) + " score is: " + tests[i]);
 		}
+		// Find the minimum test score
+		int minimumScore = tests[0];
+		for(int i = 0; i < tests.length; i++) {
+			if (tests[i] < minimumScore) {
+				minimumScore = tests[i];
+			}
+		}
+		System.out.println("The minimum test score is: " + minimumScore);
 		
+		// Find the maximum test score
+		int maximumScore = tests[0];
+		for(int i = 0; i<tests.length; i++) {
+			if(tests[i] > maximumScore) {
+				maximumScore = tests[i];
+			}
+		}
+		System.out.println("The maximum test score is: " + maximumScore);
 		
-		
+		// Find the average of the test scores entered
+		int total = 0;
+		for(int i = 0; i < tests.length; i++) {
+			total = total + tests[i];
+		}
+		int average = total / tests.length;
+		System.out.println("The average test score is: " + average);
 		
 	}
 
