22f82dfc5690b3fdb12943ea8e0c085c29df333b
afton21 <lawverap25@gmail.com>
2022-01-29 07:39:22 -0600

    Finished Bag Data Structure. Also, reformatted all eclipse files as InteliJ files by importing from Existing Source.

--- Data_Structures_1/src/Bag.java
+++ Data_Structures_1/src/Bag.java
@@ -1,10 +1,12 @@
-package src;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class Bag<T> implements BagInterface<T> {
 
-    public static void main(String[] args) {
-
-    }
+    private static final int DEFAULT_CAPACITY = 100;
+    private T[] bagElements;
+    private int numberOfElements = 0;
 
     public Bag(int capacity) {
         @SuppressWarnings("unchecked")
@@ -18,7 +20,6 @@ public class Bag<T> implements BagInterface<T> {
 
     private int getIndexOf(T input) {
         int location = -1;
-
         for (int i = 0; i < numberOfElements; i++) {
             if (input.equals(bagElements[i])) {
                 location = i;
@@ -28,48 +29,115 @@ public class Bag<T> implements BagInterface<T> {
         return location;
     }
 
-    @Override
-    public int getCurrentSize() {
-        return 0;
+    private void removeAtIndex(int index)
+    {
+        numberOfElements--;
+        bagElements[index] = bagElements[numberOfElements]; // A little confusing
+        bagElements[numberOfElements] = null;
     }
 
-    @Override
-    public boolean isFull() {
-        return false;
+    public int getCurrentSize()
+    {
+        return numberOfElements;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
+    public boolean isFull()
+    {
+        if(numberOfElements == bagElements.length)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    @Override
-    public boolean add(Object input) {
-        return false;
+    public boolean isEmpty()
+    {
+        if(numberOfElements == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    @Override
-    public Object removeRandom() {
-        return null;
+    public boolean add(T input)
+    {
+        if(isFull())
+        {
+            return false;
+        }
+        else
+        {
+            bagElements[numberOfElements] = input;
+            numberOfElements++;
+            return true;
+        }
     }
 
-    @Override
-    public boolean removeSpecific(Object input) {
-        return false;
+    public T removeRandom()
+    {
+        Random randomNumber = new Random();
+        int elementToRemove = randomNumber.nextInt(numberOfElements-1);
+        T removedElement = bagElements[elementToRemove];
+        removeAtIndex(elementToRemove);
+        return removedElement;
     }
 
-    @Override
-    public void removeAll() {
-
+    public boolean removeSpecific(T input)
+    {
+        if(isEmpty())
+        {
+            return false;
+        }
+        else {
+            int index = getIndexOf(input);
+            removeAtIndex(index);
+            return true;
+        }
     }
 
-    @Override
-    public int getCountOf(Object input) {
-        return 0;
+    public void removeAll()
+    {
+        for(int i=0; i<numberOfElements; i++)
+        {
+            bagElements[i] = null;
+        }
+        numberOfElements = 0;
     }
 
-    @Override
-    public boolean contains(Object input) {
-        return false;
+    public int getCountOf(T input)
+    {
+        int count = 0;
+
+        for(int i=0; i<numberOfElements; i++)
+        {
+            if(input.equals(bagElements[i]))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean contains(T input)
+    {
+        if(getCountOf(input) > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void doubleBagSize()
+    {
+        bagElements = Arrays.copyOf(bagElements, 2 * bagElements.length);
     }
 }
--- Data_Structures_1/src/BagInterface.java
+++ Data_Structures_1/src/BagInterface.java
@@ -1,64 +1,67 @@
+
+/**
+ * An interface that describes the operations of a bag of objects.
+ * @author Afton Lawver
+ */
+
 public interface BagInterface<T> {
-    /**
-     * An interface that describes the operations of a bag of objects.
-     * @author Afton Lawver
-     */
+
 
     /**
      * Gets the current number of entries in this bag.
      * @return The integer number of entries currently in the bag
      */
-    public int getCurrentSize();
+    int getCurrentSize();
 
     /**
      * Sees whether this bag is full.
      * @return True if the bag is full, or false if not.
      */
-    public boolean isFull();
+    boolean isFull();
 
     /**
      * Sees whether this bag is empty.
      * @return True if the bag is empty, or false if not.
      */
-    public boolean isEmpty();
+    boolean isEmpty();
 
     /**
      * Adds a new entry to this bag.
      * @param input The object to be added as a new entry.
      * @return True if the addition is successful, or false if not.
      */
-    public boolean add(T input);
+    boolean add(T input);
 
     /**
      * Removes one unspecified entry from this bag, if possible.
      * @return Either the removed entry, if the removal was successful, or null.
      */
-    public T removeRandom();
+    T removeRandom();
 
     /**
      * Removes one occurrence of a given entry from this bag, if possible.
      * @param input The entry to be removed.
      * @return True if the removal was successful, or false if not.
      */
-    public boolean removeSpecific(T input);
+    boolean removeSpecific(T input);
 
     /**
      * Removes all entries from this bag.
      */
-    public void removeAll();
+    void removeAll();
 
     /**
      * Counts the number of times a given entry appears in this bag.
      * @param input The entry to be counted.
      * @return The number of times input appears in the bag.
      */
-    public int getCountOf(T input);
+    int getCountOf(T input);
 
     /**
      * Tests whether this bag contains a given entry.
      * @param input The input to find.
      * @return True if the bag contains input, or false if not.
      */
-    public boolean contains(T input);
+    boolean contains(T input);
 }
 
