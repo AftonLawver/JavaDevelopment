0270696b12c290ac3743c1afb5c83f50226e1b23
afton21 <lawverap25@gmail.com>
2023-03-05 15:37:38 -0600

    Finished CS412 Assignment #2 Program #2. (merge sort in sequence and parallel)

--- CS412Assignment2Program2/src/MergeSortArraySeq.java
+++ CS412Assignment2Program2/src/MergeSortArraySeq.java
@@ -1,80 +1,102 @@
 // Afton Lawver
 // 851463587
 
+import java.util.Arrays;
 import java.util.Random;
 
-public class MergeSortArraySeq {
-    private int[] myArray;
-    private int numberOfElements;
+public class MergeSortArraySeq implements Runnable{
 
-    MergeSortArraySeq(int numberOfElements) {
-        this.numberOfElements = numberOfElements;
+    private int[] a;
+    private int level;
+
+    public MergeSortArraySeq(int[] a, int level) {
+        this.a = a;
+        this.level = level;
+    }
+    public void run() {
+        parallelMergeSort(a, level);
     }
 
-    public void initializeArray() {
+    public static int[] initializeArray(int numberOfElements) {
+        int[] myArray = new int[numberOfElements];
         Random rand = new Random();
-        for (int i = 0; i < this.numberOfElements; i++) {
-            int int_random = rand.nextInt(1000);
-            this.myArray[i] = int_random;
+        for (int i = 0; i < numberOfElements; i++) {
+            int int_random = rand.nextInt(numberOfElements);
+            myArray[i] = int_random;
         }
+        return myArray;
     }
 
-    public void printArray() {
+    public static void printArray(int[] myArray) {
         for(int i = 0; i < myArray.length-1; i++) {
             System.out.println(myArray[i]);
         }
     }
-    public static void merge(int[] left_arr,int[] right_arr, int[] arr,int left_size, int right_size){
 
-        int i=0,l=0,r = 0;
-        //The while loops check the conditions for merging
-        while(l<left_size && r<right_size){
-
-            if(left_arr[l]<right_arr[r]){
-                arr[i++] = left_arr[l++];
-            }
-            else{
-                arr[i++] = right_arr[r++];
-            }
-        }
-        while(l<left_size){
-            arr[i++] = left_arr[l++];
-        }
-        while(r<right_size){
-            arr[i++] = right_arr[r++];
+    public static void mergeSort(int[] a) {
+        if (a.length >= 2) {
+            // split array in half
+            int[] left  = Arrays.copyOfRange(a, 0, a.length / 2);
+            int[] right = Arrays.copyOfRange(a, a.length / 2, a.length);
+            mergeSort(left);
+            mergeSort(right);
+            merge(left, right, a);
         }
     }
 
-    public static void mergeSort(int [] arr, int len){
-        if (len < 2){return;}
-
-        int mid = len / 2;
-        int [] left_arr = new int[mid];
-        int [] right_arr = new int[len-mid];
-
-        //Dividing array into two and copying into two separate arrays
-        int k = 0;
-        for(int i = 0;i<len;++i){
-            if(i<mid){
-                left_arr[i] = arr[i];
-            }
-            else{
-                right_arr[k] = arr[i];
-                k = k+1;
+    public static void merge(int[] left, int[] right, int[] a) {
+        int i1 = 0;
+        int i2 = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (i2 >= right.length || (i1 < left.length && left[i1] < right[i2])) {
+                a[i] = left[i1];
+                i1++;
+            } else {
+                a[i] = right[i2];
+                i2++;
             }
         }
-        // Divide the subarrays
-        mergeSort(left_arr,mid);
-        mergeSort(right_arr,len-mid);
-        merge(left_arr,right_arr,arr,mid,len-mid);
     }
 
-    public static void main( String args[] ) {
-        MergeSortArraySeq myObj = new MergeSortArraySeq(10);
-        myObj.initializeArray();
-        myObj.printArray();
-//        mergeSort(array,array.length);
-//        for(int i =0; i< array.length;++i){
-//            System.out.print(array[i]+ " ");
+    public static void parallelMergeSort(int[] a) {
+        parallelMergeSort(a, 3);
+    }
+
+    private static void parallelMergeSort(int[] a, int threadCount) {
+        if (threadCount <= 1) {
+            mergeSort(a);
+        } else if (a.length >= 2) {
+            int[] left  = Arrays.copyOfRange(a, 0, a.length / 2);
+            int[] right = Arrays.copyOfRange(a, a.length / 2, a.length);
+
+            Thread lThread = new Thread(new MergeSortArraySeq(left,  threadCount / 2));
+            Thread rThread = new Thread(new MergeSortArraySeq(right, threadCount / 2));
+            lThread.start();
+            rThread.start();
+
+            try {
+                lThread.join();
+                rThread.join();
+            } catch (InterruptedException ie) {}
+
+            merge(left, right, a);
         }
     }
+
+    public static void main( String args[]) {
+        int numberOfElements = 100000;
+        int[] newArray = initializeArray(numberOfElements);
+
+        long start = System.currentTimeMillis();
+        mergeSort(newArray);
+        long end = System.currentTimeMillis();
+        System.out.println("Total time for sequential sort: " + (end - start) + "ms");
+
+        int[] newArray2 = initializeArray(numberOfElements);
+        long start2 = System.currentTimeMillis();
+        parallelMergeSort(newArray2, 5);
+        long end2 = System.currentTimeMillis();
+
+        System.out.println("Total time for parallel merge sort: " + (end2 - start2) + "ms");
+    }
+}
